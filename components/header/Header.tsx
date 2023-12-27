f1f085aba189926ac16f62883f3385321d47a0ef
Sahil Satpute <explore.sahil1@gmail.com>
2023-12-27 19:19:11 +0530

    updated header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,15 +3,33 @@
 import Link from "next/link";
 import "./style.scss";
 import { UserButton, useUser } from "@clerk/nextjs";
-import { SignInButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
+import { useLayoutEffect, useState } from "react";
 
 const Header = () => {
   const { user, isLoaded } = useUser();
   const pathname = usePathname();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useLayoutEffect(() => {
+    let prevScrollPos = window.scrollY || document.documentElement.scrollTop;
+    const handleScroll = () => {
+      const currentScrollPos =
+        window.scrollY || document.documentElement.scrollTop;
+      setIsScrolled(currentScrollPos > 0 && currentScrollPos > prevScrollPos);
+
+      prevScrollPos = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header>
+    <header className={`${isScrolled ? "scrollDown" : ""}`}>
       <Link href="/">Auth</Link>
       {isLoaded && user ? (
         <div className="account">
